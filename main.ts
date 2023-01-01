f680605dc21f2623a4b9bcde96e71ebba64dc22d
onchobot <ondra@chobot.cz>
2023-01-01 19:34:00 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 3; index++) {
         right.showColor(neopixel.colors(NeoPixelColors.Yellow))
         cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 255, 255, 0)
         basic.pause(500)
@@ -9,7 +9,7 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 3; index++) {
         left.showColor(neopixel.colors(NeoPixelColors.Yellow))
         cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 255, 0)
         basic.pause(500)
